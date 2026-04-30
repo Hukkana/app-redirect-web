a8d203ec1d054d5f4b2b7fb8ed02991c132e8df8
ふっかな <kinsei90shin@icloud.com>
2026-04-30 14:17:10 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  supabaseUrl: "https://zgztrrcjskcwlyhqmnas.supabase.co",
+  supabaseAnonKey: "sb_publishable_GRsRfV3r_6w77cqTwW_Yqw_7uYdxMrR",
+  supabaseBucket: "redirect-icons"
+};
